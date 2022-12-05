5b0161c8e44535b2ea81af3822fdd7bbbae30422
Pathan Naiem Hamid <115400703+Naiempathan01@users.noreply.github.com>
2022-12-05 22:36:44 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,35 +1,35 @@
 function renderText(domElt, text) {
     domElt.innerText = text;
-  }
-  
-  function handleWord(text) {
+}
+
+function handleWord(text) {
     const textArr = text.split(" ");
     let wordCount = 0;
-  
+
     for (word of textArr) {
-      if (/[a-zA-Z0-9]/.test(word)) {
-        wordCount += 1;
-      }
+        if (/[a-zA-Z0-9]/.test(word)) {
+            wordCount += 1;
+        }
     }
-  
+
     return wordCount;
-  }
-  
-  function handleChar(text) {
+}
+
+function handleChar(text) {
     return text.length;
-  }
-  
-  function init() {
+}
+
+function init() {
     const textArea = document.querySelector("textarea");
     const charDOM = document.querySelector("#letterCount");
     const wordDOM = document.querySelector("#wordCount");
-  
+
     textArea.addEventListener("input", (event) => {
-      const text = event.target.value.trim();
-  
-      renderText(wordDOM, handleWord(text));
-      renderText(charDOM, handleChar(text));
+        const text = event.target.value.trim();
+
+        renderText(wordDOM, handleWord(text));
+        renderText(charDOM, handleChar(text));
     });
-  }
-  
-  init();
+}
+
+init();
