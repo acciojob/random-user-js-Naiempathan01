be30484762e95b2842f463255e0289766fdc6920
Pathan Naiem Hamid <115400703+Naiempathan01@users.noreply.github.com>
2022-12-05 22:23:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
 //your code here
+function renderText(domElt, text) {
+    domElt.innerText = text;
+  }
+  
+  function handleWord(text) {
+    const textArr = text.split(" ");
+    let wordCount = 0;
+  
+    for (word of textArr) {
+      if (/[a-zA-Z0-9]/.test(word)) {
+        wordCount += 1;
+      }
+    }
+  
+    return wordCount;
+  }
+  
+  function handleChar(text) {
+    return text.length;
+  }
+  
+  function init() {
+    const textArea = document.querySelector("textarea");
+    const charDOM = document.querySelector("#letterCount");
+    const wordDOM = document.querySelector("#wordCount");
+  
+    textArea.addEventListener("input", (event) => {
+      const text = event.target.value.trim();
+  
+      renderText(wordDOM, handleWord(text));
+      renderText(charDOM, handleChar(text));
+    });
+  }
+  
+  init();
